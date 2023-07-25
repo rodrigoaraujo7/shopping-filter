e88d2ecc83e233f2cc978e4a7aee0a06197038ac
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-07-24 21:34:08 -0300

    style: global styles using stitches

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { globalStyles } from './globalStyles';
 
 function App() {
+  globalStyles()
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
---
+++ src/globalStyles.ts
@@ -0,0 +1,10 @@
+import { globalCss } from '@stitches/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+
+    margin: 0,
+    padding: 0
+  }
+})
