582a1f374f1a34bf5e8f8991d6ad419ab57fb735
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-07-27 00:34:13 -0300

    typescript: creating the structure of the filter method

--- src/components/Products/Catalog/ProductCard/index.tsx
+++ src/components/Products/Catalog/ProductCard/index.tsx
@@ -1,17 +1,18 @@
 import { Container, Image, Description, Price, Span } from './styles'
 
-interface IProductCard {
+export interface IProductCard {
   name: string,
   price: number,
-  imageURL: string
+  imageURL: string,
+  productType: 'Jacket' | 'Pants' | 'Shoes' | 'Sweater' | 'Tops'
 }
 
-const ProductCard = ({ name, price, imageURL }: IProductCard) => {
+const ProductCard = ({ name, price, imageURL, productType }: IProductCard) => {
   return (
     <Container>
       <Image src={imageURL} alt="tshirt" />
       <Description>{name} <Price>${price}</Price></Description>
-      <Span>jacket</Span>
+      <Span>{productType}</Span>
     </Container>
   )
 }
--- src/components/Products/Catalog/index.tsx
+++ src/components/Products/Catalog/index.tsx
@@ -1,12 +1,51 @@
-import ProductCard from "./ProductCard";
+import { useState, useEffect } from 'react'
+
+import ProductCard, { IProductCard } from "./ProductCard";
 
 import { CatalogContainer } from './styles'
 
 const Catalog = () => {
+  const data: IProductCard[] = [
+    {
+      name: 'Random Jacket',
+      price: 20,
+      imageURL: 'https://eadn-wc05-10739759.nxedge.io/wp-content/uploads/2023/03/tshirt-azul-2-sense.jpg',
+      productType: 'Jacket'
+    },
+    {
+      name: 'Random Pants',
+      price: 40,
+      imageURL: 'https://eadn-wc05-10739759.nxedge.io/wp-content/uploads/2023/03/tshirt-azul-2-sense.jpg',
+      productType: 'Pants'
+    },
+    {
+      name: 'Random Shoes',
+      price: 60,
+      imageURL: 'https://eadn-wc05-10739759.nxedge.io/wp-content/uploads/2023/03/tshirt-azul-2-sense.jpg',
+      productType: 'Shoes'
+    },
+  ]
+
+  const [products, setProducts] = useState<IProductCard[]>([]);
+
+  let productTypeFilter = 'Jacket';
+  let count // counter of for loop
+  useEffect(() => { // need this hook to previne too many responses
+    for (count = 0; count < data.length; count++) {
+      setProducts(data.filter(product => product.productType === productTypeFilter))
+    }
+  }, [count])
+
   return (
     <CatalogContainer>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(product => (
-        <ProductCard name="T-Shirt" price={85} imageURL="https://eadn-wc05-10739759.nxedge.io/wp-content/uploads/2023/03/tshirt-azul-2-sense.jpg" />
+      {products.map((product, index) => (
+        <ProductCard
+          key={index}
+          name={product.name}
+          price={product.price}
+          imageURL={product.imageURL}
+          productType={product.productType}
+        />
       ))}
     </CatalogContainer>
   )
